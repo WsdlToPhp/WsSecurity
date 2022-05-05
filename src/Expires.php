a9db8521ae60240172cd196ad053a5eadd9ba66a
liquid207 <im.shuai.yang@outlook.com>
2022-05-05 15:10:19 +0800

    Change wssu namespace to wsu

--- src/Expires.php
+++ src/Expires.php
@@ -8,7 +8,7 @@ class Expires extends Element
 {
     public const NAME = 'Expires';
 
-    public function __construct(int $timestamp, int $expiresIn = 3600, string $namespace = self::NS_WSSU)
+    public function __construct(int $timestamp, int $expiresIn = 3600, string $namespace = self::NS_WSU)
     {
         $this->setTimestampValue($timestamp + $expiresIn);
         parent::__construct(self::NAME, $namespace, $this->getTimestampValue(true));
